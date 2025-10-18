932b162b13212230edb2febe0667d1e40e5c2216
Hyun Jun <154495800+IlLa00@users.noreply.github.com>
2025-10-18 18:45:03 +0900

    1018 18:44
    런타임데이터 작업중

---
+++ Assets/Scripts/Data/Runtime/BaseRuntimeData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+public abstract class BaseRuntimeData : ScriptableObject
+{
+    [SerializeField]
+    public string filename;
+
+    public event Action OnValueChanged;
+
+    protected void NotifyValueChanged()
+    {
+        OnValueChanged?.Invoke();
+    }
+
+    public abstract void ResetData();
+
+    virtual public void SaveData()
+    {
+        AssetDatabase.CreateAsset(this, "Data/" + filename + ".asset");
+        AssetDatabase.SaveAssets();
+    }
+}
---
+++ Assets/Scripts/ScriptData/RoguelikeScriptData.cs
@@ -0,0 +1,17 @@
+﻿[System.Serializable]
+public class RoguelikeScriptData
+{
+    public string name;
+    public string description;
+    public string stat;
+    public int gold;
+
+    public RoguelikeScriptData(string name, string description, string stat, int gold)
+    {
+        this.name = name;
+        this.description = description;
+        this.stat = stat;
+        this.gold = gold;
+    }
+}
+              
